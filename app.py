c9eb0387a2c1915d409e340956d8a7c7c42d964e
Hinoki-Sugita <0601yoichiro0601@gmail.com>
2026-05-15 10:22:32 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import time
 
+two   = st.checkbox("2人")
+three = st.checkbox("3人")
+four  = st.checkbox("4人")
+five  = st.checkbox("5人")
+
 placeholder = st.empty()
 
 for remaining in range(20, -1, -1):
@@ -11,5 +16,7 @@ for remaining in range(20, -1, -1):
         placeholder.write("終了")
     else:
         placeholder.write(f"{minutes:02}:{seconds:02}")
-
     time.sleep(1)
+
+    if remaining == 10:
+        st.write("10秒")
